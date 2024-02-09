6d44300d338c9dabe9bca61d7f099fbdb750bc34
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2024-02-09 21:15:51 +0530

    router created

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { config } from "dotenv";
+import morgan from "morgan";
+import appRouter from "./routes/index.js";
 config(); // load .env
 const app = express();
 // using JSON for req and res
 app.use(express.json());
+app.use(morgan("dev"));
+app.use("/api/v1", appRouter);
 export default app;
 //# sourceMappingURL=app.js.map
---
+++ backend/src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import userRouter from './user-routes.js';
+import chatRoutes from './chat-routes.js';
+
+const appRouter = Router();
+
+appRouter.use('/users', userRouter);
+appRouter.use('/chats', chatRoutes);
+
+export default appRouter;
